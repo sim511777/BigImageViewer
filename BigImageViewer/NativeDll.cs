66db0ad7374c41954b2798892bed15a42e5b160a
sim511777 <sim51177@gmail.com>
2020-01-04 21:10:01 +0900

    NativeImport 프로젝트 제거

--- BigImageViewer/NativeDll.cs
+++ BigImageViewer/NativeDll.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NativeImport {
+namespace BigImageViewer {
     public class NativeDll {
         const string dll = "native.dll";
         [DllImport(dll)] public static extern bool Load8BitBmp(IntPtr buf, int bw, int bh, string filePath);
