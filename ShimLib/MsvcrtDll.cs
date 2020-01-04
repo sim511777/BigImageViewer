237cc59a737bcca544a67f0a1e476f9ac6b6a153
sim511777 <sim51177@gmail.com>
2020-01-04 21:16:18 +0900

    MsvcrtDll클래스 dll파일명 에러 수정

--- ShimLib/MsvcrtDll.cs
+++ ShimLib/MsvcrtDll.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace ShimLib {
     public class MsvcrtDll {
         const string dll = "msvcrt.dll";
-        [DllImport("dll")] public static extern IntPtr memcpy(IntPtr _Dst, IntPtr _Src, long _Size);
-        [DllImport("dll")] public static extern IntPtr memset(IntPtr _Dst, int _Val, long _Size);
+        [DllImport(dll)] public static extern IntPtr memcpy(IntPtr _Dst, IntPtr _Src, long _Size);
+        [DllImport(dll)] public static extern IntPtr memset(IntPtr _Dst, int _Val, long _Size);
     }
 }
